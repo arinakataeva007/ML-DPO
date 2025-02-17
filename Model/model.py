52724c9a8d8f9e6c7632e398410cf2fe90371a05
Arina Kataeva <123296525+arinakataeva007@users.noreply.github.com>
2025-02-17 21:48:01 +0500

    feat(feat): v2 accuracy 0.49
    works initial version

--- Model/model.py
+++ Model/model.py
@@ -2,10 +2,15 @@ import numpy as np
 import matplotlib.pyplot as plt
 import streamlit as st # для создания веб-приложения
 from PIL import Image # для работы с разными расширениями картинок
+import tensorflow as tf
 from tensorflow.keras.datasets import cifar10 # набор данных (60К данных 10 классов)
 from tensorflow.keras.models import Sequential # класс для создания слоев нейросети
-from tensorflow.keras.layers import Flaten, Dense # класс для создания полносвязных слоев нейросети
+from tensorflow.keras.layers import Flatten, Dense # класс для создания полносвязных слоев нейросети
 from tensorflow.keras.utils import to_categorical # преобразует вектор в матрицу двоичных классов
+import ssl
+
+# Отключаем проверку SSL
+ssl._create_default_https_context = ssl._create_unverified_context
 
 def create_model():
     # распаковка на тренировочную-валидационную-тестовую выборки
@@ -24,11 +29,10 @@ def create_model():
         Dense(1000, activation='relu'), # полносвязный слой с функцией активации релу
         Dense(10, activation='softmax') # 10 - нейронов, потому что 10 классов, каждый из 10 нейронов будет активирован от 0-100: вероятность принадлежности к классу
     ])
-    model.compile(loss='categorical_crossentropy', optimizer='adam', metric=['accuracy']) # лос-вычисляет насколько наше распределение вероятностей далеко от правды
+    model.compile(loss='categorical_crossentropy', optimizer='adam', metrics=['accuracy']) # лос-вычисляет насколько наше распределение вероятностей далеко от правды
     model.fit(x_train, y_train, batch_size=64, epochs=10, validation_data=(x_val, y_val)) # тренируем массив, batch_size - пачка картинок для тренировки, валидационные данные для проверки
     model.save('veights_neural.h5')
 
 
 # Обучаем модель, только если запускается файл
-if __name__ == 'main':
-    create_model()
+create_model()
